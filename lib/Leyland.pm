b80bab92044f68c7f052563b8e3191e16dc12e5e
Ido Perlmuter <ido@ido50.net>
2011-04-18 19:08:43 +0300

    shorter synopsis

--- lib/Leyland.pm
+++ lib/Leyland.pm
@@ -24,65 +24,24 @@ Leyland - Plack-based framework for RESTful web applications
 
 =head1 SYNOPSIS
 
-	# app.psgi:
-	# --------------------------------------------------------------
+	# in app.psgi:
+
 	#!/usr/bin/perl -w
 
 	use strict;
 	use warnings;
 	use MyApp;
-	use Plack::Builder;
 
 	my $config = {
 		app => 'MyApp',
 		views => ['Tenjin'],
 		locales => './i18n',
-		logger => {
-			class => 'LogHandler',
-			opts => {
-				outputs => [
-					file => {
-						filename => "myapp.$ENV{PLACK_ENV}.log",
-						minlevel => 0,
-						maxlevel => 8,
-						utf8 => 1,
-					},
-					screen => {
-						log_to   => "STDERR",
-						minlevel => 0,
-						maxlevel => 8,
-					},
-				]
-			}
-		},
-		environments => {
-			development => {
-				# options in here will override top level options when running in the development environment
-			},
-			deployment => {
-				# options in here will override top level options when running in the deployment environment
-			},
-		}
 	};
 
 	my $myapp = MyApp->new(config => $config);
 
 	my $app = sub {
 		$myapp->handle(shift);
-	};
-
-	builder {
-		# enable whatever Plack middlewares you wish here, a good example
-		# would be the Session middleware.
-		# --------------------------------------------------------------
-		# the Static middleware will serve static files from the app's
-		# public directory, remove it (or comment it) if your web server
-		# is serving those files
-		enable 'Static',
-			path => qr{^/((images|js|css|fonts)/|favicon\.ico$|apple-touch-icon\.png$)},
-			root => './public/';
-
-		$app;
 	};
 
 =head1 DESCRIPTION
